f340690e9b35c3d02b1b22bfb51d0f170336cc2a
William Mortl <wmortl@Williams-MBP.hsd1.co.comcast.net>
2019-08-08 09:22:09 -0600

    removed setvars

--- keyvault/key.go
+++ keyvault/key.go
@@ -4,18 +4,21 @@ import (
 	"context"
 
 	"github.com/WilliamMortlMicrosoft/AzureGoSamples/internal/config"
-	"github.com/WilliamMortlMicrosoft/AzureGoSamples/internal/iam"
+	//"github.com/WilliamMortlMicrosoft/AzureGoSamples/internal/iam"
 
 	"github.com/Azure/azure-sdk-for-go/services/keyvault/v7.0/keyvault"
 	"github.com/Azure/go-autorest/autorest/to"
 )
 
-func getKeysClient() keyvault.BaseClient {
+func getKeysClient() (keyvault.BaseClient, error) {
 	keyClient := keyvault.New()
-	a, _ := iam.GetKeyvaultAuthorizer()
-	keyClient.Authorizer = a
-	keyClient.AddToUserAgent(config.UserAgent())
-	return keyClient
+	var err error
+	//a, err := iam.GetKeyvaultAuthorizer()
+	//if (err == nil) {
+	//	keyClient.Authorizer = a
+		keyClient.AddToUserAgent(config.UserAgent())
+	//}
+	return keyClient, err
 }
 
 // CreateKeyBundle creates a key in the specified keyvault
@@ -27,24 +30,29 @@ func CreateKey(ctx context.Context, vaultName, keyName string) (key keyvault.Key
 	}
 	vaultURL := *vault.Properties.VaultURI
 
-	keyClient := getKeysClient()
-	return keyClient.CreateKey(
-		ctx,
-		vaultURL,
-		keyName,
-		keyvault.KeyCreateParameters{
-			KeyAttributes: &keyvault.KeyAttributes{
-				Enabled: to.BoolPtr(true),
-			},
-			KeySize: to.Int32Ptr(2048), // As of writing this sample, 2048 is the only supported KeySize.
-			KeyOps: &[]keyvault.JSONWebKeyOperation{
-				keyvault.Encrypt,
-				keyvault.Decrypt,
-				keyvault.Sign,
-				keyvault.Verify,
-				keyvault.WrapKey,
-				keyvault.UnwrapKey,
-			},
-			Kty: keyvault.RSA,
-		})
+	var response keyvault.KeyBundle
+	keyClient, err := getKeysClient()
+	if (err == nil) {
+		response, err = keyClient.CreateKey(
+			ctx,
+			vaultURL,
+			keyName,
+			keyvault.KeyCreateParameters{
+				KeyAttributes: &keyvault.KeyAttributes{
+					Enabled: to.BoolPtr(true),
+				},
+				KeySize: to.Int32Ptr(2048), // As of writing this sample, 2048 is the only supported KeySize.
+				KeyOps: &[]keyvault.JSONWebKeyOperation{
+					keyvault.Encrypt,
+					keyvault.Decrypt,
+					keyvault.Sign,
+					keyvault.Verify,
+					keyvault.WrapKey,
+					keyvault.UnwrapKey,
+				},
+				Kty: keyvault.RSA,
+			})
+	}
+
+	return response, err
 }
